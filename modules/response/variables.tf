a58859b4f44362e9d93372cf667ef281a4944dd7
Dotan Alter <dotan@stream.security>
2025-06-03 13:22:26 +0300

    add response module

---
+++ modules/response/variables.tf
@@ -0,0 +1,31 @@
+################################################################################
+# Stream Security AWS Account Variables
+################################################################################
+
+variable "streamsec_account" {
+  description = "The AWS Account ID for the Stream.Security account."
+  type        = string
+  default     = "624907860825"
+}
+
+variable "response_policy_name" {
+  description = "The name of the response policy"
+  type        = string
+  default     = "stream-security-response-policy"
+}
+
+variable "runbooks_prefix" {
+  description = "The prefix for the runbooks"
+  type        = string
+  default     = ""
+}
+
+################################################################################
+# General
+################################################################################
+
+variable "tags" {
+  description = "A map of global tags to add to all created resources"
+  type        = map(string)
+  default     = {}
+}
