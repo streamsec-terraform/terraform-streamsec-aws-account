e113d87c0a735be21fc4289aacbeaf011c83d8e6
Ofir Machluf <ofir.machluf@gmail.com>
2025-08-21 11:16:11 +0300

    add support private link

--- modules/private-link/local.tf
+++ modules/private-link/local.tf
@@ -1,5 +1,3 @@
 locals {
   _pl_sg_id = var.privatelink_security_group_id != null ? var.privatelink_security_group_id : (var.enable_privatelink ? aws_security_group.streamsec_privatelink[0].id : null)
-
-    streamsec_api_url = (var.enable_privatelink && try(length(var.privatelink_service_name) > 0, false)) ? var.privatelink_service_name : data.streamsec_host.this.url
 }
