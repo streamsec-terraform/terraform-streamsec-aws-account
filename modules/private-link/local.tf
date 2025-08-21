d855489716878a185f4403aa891a5e7b2a942492
Ofir Machluf <ofir.machluf@gmail.com>
2025-08-21 12:24:55 +0300

    add support private link

--- modules/private-link/local.tf
+++ modules/private-link/local.tf
@@ -1,3 +1,4 @@
 locals {
   _pl_sg_id = var.privatelink_security_group_id != null ? var.privatelink_security_group_id : (var.enable_privatelink ? aws_security_group.streamsec_privatelink[0].id : null)
+  lightlytics_dns = [ for entry in aws_vpc_endpoint.streamsec.dns_entry : entry.dns_name if can(regex("lightlytics\\.com$", entry.dns_name))][0]
 }
