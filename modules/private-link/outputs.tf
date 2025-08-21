98dd234f15b641e1198e163cfc3dc466b48c5ac5
Ofir Machluf <ofir.machluf@gmail.com>
2025-08-21 10:19:19 +0300

    add support private link

---
+++ modules/private-link/outputs.tf
@@ -0,0 +1,18 @@
+################################################################################
+# Private Link
+################################################################################
+
+output "privatelink_vpce_id" {
+  value       = try(aws_vpc_endpoint.streamsec[0].id, null)
+  description = "The Interface VPC Endpoint ID"
+}
+
+output "privatelink_dns_entries" {
+  value       = try(aws_vpc_endpoint.streamsec[0].dns_entry, null)
+  description = "DNS entries for the endpoint (includes the private hosted zone name when private DNS is enabled)"
+}
+
+output "privatelink_security_group_id" {
+  value       = local._pl_sg_id
+  description = "Security Group used for PrivateLink"
+}
--- modules/real-time-events/variables.tf
+++ modules/real-time-events/variables.tf
@@ -163,3 +163,49 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+################################################################################
+# Private Link
+################################################################################
+
+variable "enable_privatelink" {
+  description = "Create an Interface VPC Endpoint for StreamSecurity"
+  type        = bool
+  default     = false
+}
+
+variable "privatelink_service_name" {
+  description = "StreamSecurity AWS PrivateLink service name for your region (from StreamSecurity)"
+  type        = string
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the Interface Endpoint will be created"
+  type        = string
+  default     = null
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet IDs for the Interface Endpoint"
+  type        = list(string)
+  default     = []
+}
+
+variable "privatelink_security_group_id" {
+  description = "Optional existing SG ID allowing egress 443 to the endpoint. If null, the module creates one."
+  type        = string
+  default     = null
+}
+
+variable "enable_privatelink_private_dns" {
+  description = "Enable Private DNS on the VPC Endpoint (recommended)"
+  type        = bool
+  default     = true
+}
+
+variable "privatelink_tags" {
+  description = "Tags for PrivateLink resources"
+  type        = map(string)
+  default     = {}
+}
