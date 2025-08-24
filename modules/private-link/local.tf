66086b51f13c46e2042408735a2497b8ef9a2aff
Dotan Alter <dotan@stream.security>
2025-08-24 13:31:51 +0300

    take forked branch from kaltura and fix issues

--- modules/private-link/local.tf
+++ modules/private-link/local.tf
@@ -1,4 +1,4 @@
 locals {
   _pl_sg_id       = var.privatelink_security_group_id != null ? var.privatelink_security_group_id : (var.enable_privatelink ? aws_security_group.streamsec_privatelink[0].id : null)
-  lightlytics_dns = [for entry in aws_vpc_endpoint.streamsec[0].dns_entry : entry.dns_name if can(regex("lightlytics\\.com$", entry.dns_name))][0]
+  lightlytics_dns = var.enable_privatelink ? [for entry in aws_vpc_endpoint.streamsec[0].dns_entry : entry.dns_name if can(regex("lightlytics\\.com$", entry.dns_name))][0] : null
 }
